eb2bb3141a8e678f10ddf29496d64a4e7561fc5c
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-04-26 14:33:12 +0200

    Refactored folder structure

--- src/MovieManagement/Program.cs
+++ src/MovieManagement/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using MovieManagement.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
