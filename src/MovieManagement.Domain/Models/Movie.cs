db647e6841e84d9e978a55831b3d5b77a0936287
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-04 13:59:56 +0200

    SEP6-57: Added image for movies

--- src/MovieManagement.Domain/Models/Movie.cs
+++ src/MovieManagement.Domain/Models/Movie.cs
@@ -3,10 +3,11 @@
 public class Movie
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string Title { get; set; } = string.Empty;
     public int Revenue { get; set; }
     public DateTime ReleaseDate { get; set; }
     public int Budget { get; set; }
-    public string Description { get; set; }
+    public string Description { get; set; } = string.Empty;
     public bool IsAdult { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
 }
--- src/MovieManagement.TMDB.Api/Constants/Variables.cs
+++ src/MovieManagement.TMDB.Api/Constants/Variables.cs
@@ -3,6 +3,7 @@
 public class Variables
 {
     public const string MovieDatabaseUri = "https://api.themoviedb.org/3/";
+    public const string ImageUri = "https://image.tmdb.org/t/p/original";
     public const string MoviePath = "movie/";
     public const string UpcomingPath = "upcoming";
     public const string ApiPath = "?api_key=";
