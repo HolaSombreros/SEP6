14cb07b7c2f29d478319aa530b8c8f24a260a4a3
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-04 12:43:44 +0200

    SEP6-57: Updated nullable properties

--- src/MovieManagement.TMDB.Api/Dtos/MovieDto.cs
+++ src/MovieManagement.TMDB.Api/Dtos/MovieDto.cs
@@ -5,13 +5,13 @@ public class MovieDto
     public int Id { get; set; }
     public int Budget { get; set; }
     public int Revenue { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     [JsonPropertyName("release_date")]
-    public string ReleaseDate { get; set; }
+    public string? ReleaseDate { get; set; }
     [JsonPropertyName("vote_average")]
     public double VoteAverage { get; set; }
     [JsonPropertyName("overview")]
-    public string Description { get; set; }
+    public string? Description { get; set; }
     [JsonPropertyName("adult")]
     public bool IsAdult { get; set; }
 }
