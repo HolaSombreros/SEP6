cf13653a36421a1029ca1bc973eaf34fd31c0a8f
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-10 13:54:04 +0200

    SEP61: Added null image for crew and cast in case the api has none

--- src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
+++ src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
@@ -4,12 +4,12 @@ public class MovieMapper : Profile
 {
     public MovieMapper()
     {
-        CreateMap<MovieDto, Movie>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => ApiConfig.ImageUri + src.ImageUrl));
+        CreateMap<MovieDto, Movie>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
         CreateMap<MovieListDto, MovieList>().ForMember(dest => dest.ListType, opt => opt.MapFrom(src => "Upcoming"));
         CreateMap<DatePeriodDto, DatePeriod>();
         CreateMap<GenreDto, Genre>();
-        CreateMap<CastDto, Cast>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => ApiConfig.ImageUri + src.ImageUrl));;
-        CreateMap<CrewDto, Crew>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => ApiConfig.ImageUri + src.ImageUrl));;
+        CreateMap<CastDto, Cast>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
+        CreateMap<CrewDto, Crew>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
         CreateMap<CreditsDto, Credits>();
         CreateMap<ProductionCompanyDto, ProductionCompany>();
         CreateMap<ProductionCountryDto, ProductionCountry>();
