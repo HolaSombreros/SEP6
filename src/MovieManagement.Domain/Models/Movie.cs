5e9c9730a477fec12716bf02976c8df3f4596308
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-10 13:18:23 +0200

    SEP-61: Added properties for movie details

--- src/MovieManagement.Domain/Models/Movie.cs
+++ src/MovieManagement.Domain/Models/Movie.cs
@@ -11,4 +11,10 @@ public class Movie
     public bool IsAdult { get; set; }
     public string ImageUrl { get; set; } = default!;
     public IList<Genre> Genres { get; set; } = default!;
+    public int VoteCount { get; set; }
+    public double VoteAverage { get; set; }
+    public int Length { get; set; }
+    public double Popularity { get; set; }
+    public string OriginalLanguage { get; set; } = default!;
+    public string Status { get; set; } = default!;
 }
