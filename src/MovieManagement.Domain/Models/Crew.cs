e3ce48fd52bb65662d62cd5d7eded7b641c6bb2e
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-11 23:31:16 +0200

    SEP61: Update Crew with Role and director

--- src/MovieManagement.Domain/Models/Crew.cs
+++ src/MovieManagement.Domain/Models/Crew.cs
@@ -5,7 +5,7 @@ public class Crew
     public int Id { get; set; }
     public string Department { get; set; } = default!;
     public string Name { get; set; } = default!;
-    public string Job { get; set; } = default!;
+    public Role Role { get; set; } = default!;
     public double Popularity { get; set; }
     public string OriginalName { get; set; } = default!;
     public int Gender { get; set; }
--- src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
+++ src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
@@ -9,7 +9,8 @@ public class MovieMapper : Profile
         CreateMap<DatePeriodDto, DatePeriod>();
         CreateMap<GenreDto, Genre>();
         CreateMap<CastDto, Cast>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
-        CreateMap<CrewDto, Crew>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
+        CreateMap<CrewDto, Crew>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl))
+            .ForMember(dest => dest.Role, opt => opt.MapFrom(src => string.Equals(src.Job, Role.DIRECTOR.ToString(), StringComparison.OrdinalIgnoreCase) ? Role.DIRECTOR : Role.OTHER));
         CreateMap<CreditsDto, Credits>();
         CreateMap<ProductionCompanyDto, ProductionCompany>();
         CreateMap<ProductionCountryDto, ProductionCountry>();
