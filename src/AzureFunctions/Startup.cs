4fc91cd5fc11e7b3498736ade77eb75fefa87152
Sombrero <adriana@grecea.net>
2023-05-12 09:59:02 +0200

    SEP6-18: refactor startup

--- src/AzureFunctions/Startup.cs
+++ src/AzureFunctions/Startup.cs
@@ -3,10 +3,14 @@ namespace AzureFunctions;
 
 public class Startup : FunctionsStartup
 {
+    
     public override void Configure(IFunctionsHostBuilder builder)
     {
+        var config = new ConfigurationBuilder().AddEnvironmentVariables().Build();
+        
         builder.Services.AddScoped<IUserService,UserService>();
-        builder.Services.AddDbContext<MoviemanagementDbContext>();
+        builder.Services.AddDbContext<MoviemanagementDbContext>(options =>
+            options.UseSqlServer(config["DbConnectionString"]));
         builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
     }
 }
