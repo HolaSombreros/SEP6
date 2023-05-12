44ca8810d3b57e3f040f1a899a722353fe93e2aa
Sombrero <adriana@grecea.net>
2023-05-12 10:55:18 +0200

    SEP6-18: refactor model

--- src/MovieManagement.Domain/Models/MovieList.cs
+++ src/MovieManagement.Domain/Models/MovieList.cs
@@ -2,10 +2,10 @@
 
 public class MovieList
 {
-    public int? TotalPages { get; set; }
-    public int? TotalResults { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalResults { get; set; }
     public DatePeriod Dates { get; set; } = default!;
     public List<Movie> Movies { get; set; } = default!;
-    public int? Page { get; set; }
-    public string? ListType { get; set; }
+    public int Page { get; set; }
+    public ListType ListType { get; set; }
 }
