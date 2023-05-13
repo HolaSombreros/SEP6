04294319a2cf7de54142c1dfe2a084e4d2c26d03
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-13 13:46:11 +0200

    SEP-61: Added logo path to production company

--- src/MovieManagement.Domain/Models/ProductionCompany.cs
+++ src/MovieManagement.Domain/Models/ProductionCompany.cs
@@ -4,4 +4,5 @@ public class ProductionCompany
 {
     public string Name { get; set; } = default!;
     public string Country { get; set; } = default!;
+    public string LogoPath { get; set; } = default!;
 }
