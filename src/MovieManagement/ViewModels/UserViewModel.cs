1e8c59a03c00b7a55abdb5e2b3ce4b26da929a61
Vlad <dirlea.ioan.vlad@gmail.com>
2023-05-14 13:17:39 +0200

    SEP6-64: Userviewmodel annotations refactor

--- src/MovieManagement/ViewModels/UserViewModel.cs
+++ src/MovieManagement/ViewModels/UserViewModel.cs
@@ -7,19 +7,19 @@ public class UserViewModel
     public Guid UserId { get; set; }
     
     [Required(ErrorMessage = "Please enter a username")]
-    [MaxLength(50, ErrorMessage = "The username cannot exceed 50 characters")]
-    [MinLength(3, ErrorMessage = "The username has to be longer than 3 characters")]
+    [MaxLength(50, ErrorMessage = "The username cannot exceed {1} characters")]
+    [MinLength(3, ErrorMessage = "The username has to be at least {1} characters")]
     public string Username { get; set; } = default!;
     
     [DataType(DataType.EmailAddress)]
     [Required(ErrorMessage = "Please enter an email address")]
     [EmailAddress(ErrorMessage = "Please enter a valid email")]
-    [MaxLength(50, ErrorMessage = "The email cannot exceed 50 characters")]
+    [MaxLength(50, ErrorMessage = "The email cannot exceed {1} characters")]
     public string Email { get; set; } = default!;
     
     [Required(ErrorMessage = "Please enter a password")]
-    [MaxLength(50, ErrorMessage = "The password cannot exceed 50 characters")]
-    [MinLength(6, ErrorMessage = "The password has to be longer than 6 characters")]
+    [MaxLength(50, ErrorMessage = "The password cannot exceed {1} characters")]
+    [MinLength(6, ErrorMessage = "The password has to be at least {1} characters")]
     public string Password { get; set; } = default!;
 
     [Required(ErrorMessage = "Please confirm the password")]
