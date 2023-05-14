07f860167071a3db95cc7d4e7fb38ee4bc916b15
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-14 10:00:45 +0200

    Review comments

--- src/MovieManagement/Shared/MovieCard.razor.cs
+++ src/MovieManagement/Shared/MovieCard.razor.cs
@@ -5,19 +5,19 @@ namespace MovieManagement.Shared;
 
 public partial class MovieCard : ComponentBase
 {
-	[Parameter]
-	public MovieViewModel Movie { get; set; } = default!;
+  [Parameter]
+  public MovieViewModel Movie { get; set; } = default!;
 
-	[Inject]
-	public NavigationManager NavigationManager { get; set; } = default!;
+  [Inject]
+  public NavigationManager NavigationManager { get; set; } = default!;
 
-	protected override void OnInitialized()
-	{
-		Movie.PosterPath = "https://image.tmdb.org/t/p/w500" + Movie.PosterPath;
-	}
+  protected override void OnInitialized()
+  {
+    Movie.PosterPath = "https://image.tmdb.org/t/p/w500" + Movie.PosterPath;
+  }
 
-	private void ShowMovieDetails(long movieId)
-	{
-		NavigationManager.NavigateTo($"/movies/all/{movieId}");
-	}
+  private void ShowMovieDetails(long movieId)
+  {
+    NavigationManager.NavigateTo($"/movies/all/{movieId}");
+  }
 }
