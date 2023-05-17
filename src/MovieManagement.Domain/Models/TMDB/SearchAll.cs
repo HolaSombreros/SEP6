9880084e5e771e3d6f50885ea64d7444fb66f602
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-17 09:37:50 +0200

    Revert "Merge pull request #21 from HolaSombreros/SEP6-11-Search-for-movies"
    This reverts commit 6e8d32aefe8c6d7bc60b4345d15dd4a964be14a9, reversing
    changes made to 04b15e15d746264730dd22eb73acd94b9951687a.

--- src/MovieManagement.Domain/Models/TMDB/SearchAll.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MovieManagement.Domain.Models.TMDB;
-
-public class SearchAll
-{
-    public int TotalPages { get; set; }
-    public int TotalResults { get; set; }
-    public List<SearchResult> Results { get; set; } = default!;
-    public int Page { get; set; }
-}
