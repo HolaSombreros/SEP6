1695fc7c2bd54cf8e63d7de250a0735f8e125b0b
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-17 11:29:27 +0200

    Add search bar

--- src/MovieManagement/Shared/Sidebar.razor.cs
+++ src/MovieManagement/Shared/Sidebar.razor.cs
@@ -1,24 +1,33 @@
-﻿namespace MovieManagement.Shared;
+﻿using MovieManagement.ViewModels.Search;
+
+namespace MovieManagement.Shared;
 
 public partial class Sidebar : ComponentBase
 {
-  private bool hideSidebar = true;
-  private string? SidebarCssClass => hideSidebar ? "hide-sidebar" : null;
-  private List<MovieListViewModel>? customMovieLists;
+    private bool hideSidebar = true;
+    private string? SidebarCssClass => hideSidebar ? "hide-sidebar" : null;
+    private List<MovieListViewModel>? customMovieLists;
+    private SearchViewModel searchModel = new();
 
-  protected override void OnInitialized()
-  {
-    customMovieLists = DummyData.GetCustomMovieLists();
-    // TODO - Implement me.
-  }
+    protected override void OnInitialized()
+    {
+        customMovieLists = DummyData.GetCustomMovieLists();
+        // TODO - Implement me.
+    }
 
-  private void ToggleSidebar()
-  {
-    hideSidebar = !hideSidebar;
-  }
+    private void ToggleSidebar()
+    {
+        hideSidebar = !hideSidebar;
+    }
 
-  private void CreateNewMovieList()
-  {
-    // TODO - Implement me.
-  }
+    private void CreateNewMovieList()
+    {
+        // TODO - Implement me.
+    }
+
+    private async Task SearchAsync()
+    {
+        // TODO: Call SearchService.
+        searchModel = new();
+    }
 }
---
+++ src/MovieManagement/ViewModels/Search/SearchViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieManagement.ViewModels.Search;
+
+public class SearchViewModel
+{
+    [MinLength(3, ErrorMessage = "Minimum {1} chacters")]
+    public string SearchInput { get; set; } = "";
+}
