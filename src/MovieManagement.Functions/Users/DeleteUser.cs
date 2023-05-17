eed18da421a0bff0172590fb3e360fd16f8e0ea2
Sombrero <adriana@grecea.net>
2023-05-17 17:03:03 +0200

    SEP6-90: refactor function to delete http method

--- src/MovieManagement.Functions/Users/DeleteUser.cs
+++ src/MovieManagement.Functions/Users/DeleteUser.cs
@@ -9,15 +9,12 @@ public class DeleteUser {
 
     [FunctionName("DeleteUser")]
     public async Task<IActionResult> RunAsync(
-        [HttpTrigger(AuthorizationLevel.Function,  "post", Route = null)] HttpRequest req, ILogger log) {
+        [HttpTrigger(AuthorizationLevel.Function,  nameof(HttpMethods.Delete), Route = "DeleteUser/{userId}")] HttpRequest req, [FromRoute]string userId, ILogger log) {
         try 
         {
             log.LogInformation("C# HTTP trigger function processed a request");
-            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-            var userId = JsonConvert.DeserializeObject<DeleteUserDto>(requestBody);
-            
-            await _userService.DeleteUser(userId.Id);
+            var guid = Guid.Parse(userId);
+            await _userService.DeleteUser(guid);
             log.LogInformation("Deleted user with id " + userId);
             return new OkResult();
         }
