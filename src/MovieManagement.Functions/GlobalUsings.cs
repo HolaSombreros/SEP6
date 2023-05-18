cc31450ffcfc6adeb8c4cedf45d3d67c05318775
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-18 15:07:26 +0200

    SEP6-24: Remove duplicate global using

--- src/MovieManagement.Functions/GlobalUsings.cs
+++ src/MovieManagement.Functions/GlobalUsings.cs
@@ -20,7 +20,6 @@ global using MovieManagement.Database.Repositories;
 global using MovieManagement.Database.Entities;
 global using FluentValidation;
 global using MovieManagement.Functions.Validators;
-global using MovieManagement.Database.Entities;
 global using MovieManagement.Functions.Mappers;
 global using System.Collections.Generic;
 global using System.Linq;
