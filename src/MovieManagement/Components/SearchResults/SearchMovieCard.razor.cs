dfaa6387ad6a63cdb26037fe229be59914a09e2e
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-18 08:58:33 +0200

    Review comments

---
+++ src/MovieManagement/Components/SearchResults/SearchMovieCard.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace MovieManagement.Components.SearchResults;
+
+public partial class SearchMovieCard : ComponentBase
+{
+    [Parameter]
+    public SearchMovieViewModel Movie { get; set; } = default!;
+
+    private void ShowMovieDetails(long movieId)
+    {
+        NavigationManager.NavigateTo($"/movies/{movieId}");
+    }
+}
