2e9cbb6ea9c8ea1e11b16aef78e8f23228d218df
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-19 11:53:12 +0200

    SEP6-42: Specific http client for statistics

--- src/MovieManagement.TMDB.Api/Services/StatisticsService.cs
+++ src/MovieManagement.TMDB.Api/Services/StatisticsService.cs
@@ -17,13 +17,31 @@ public class StatisticsService : IStatisticsService
     {
         try
         {
-            var endpoint = _settings.DiscoverPath +
-                           _settings.MoviePath + "?include_adult=false&include_video=false&page=" +
-                           page + "&primary_release_year=" + year + "&sort_by=vote_count.desc" +
-                           _settings.AndQueryBuilder;
-            var movieListDto = await _service.GetAsync<MovieListDto>(endpoint);
-            return _movieMapper.Map<MovieListDto, MovieList>(movieListDto);
+             var endpoint = _settings.DiscoverPath +
+                            _settings.MoviePath + "?include_adult=false&include_video=false&primary_release_year=" +
+                            year + "&language=en-US&page=" + page + "&sort_by=vote_count.desc" +
+                            _settings.AndQueryBuilder;
+             //var movieListDto = await _service.GetAsync<MovieListDto>(endpoint);
 
+            var client = new HttpClient();
+            var movieListDto = new MovieListDto();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&primary_release_year=2022&language=en-US&page=1&sort_by=vote_count.desc&api_key=ea9636733ed6b1f42e788b4147b59d6a"),
+                Headers =
+                {
+                    { "accept", "application/json" }
+                }
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                movieListDto = JsonSerializer.Deserialize<MovieListDto>(body);
+            }
+            return _movieMapper.Map<MovieListDto, MovieList>(movieListDto);
+            
         }
         catch
         {
