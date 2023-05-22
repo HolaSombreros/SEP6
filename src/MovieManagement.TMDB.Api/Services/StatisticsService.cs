b60ea0e38d12872e7d5494f6f30c6994725b00e5
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-22 21:43:46 +0200

    SEP6-45: Most popular movies of an actor

--- src/MovieManagement.TMDB.Api/Services/StatisticsService.cs
+++ src/MovieManagement.TMDB.Api/Services/StatisticsService.cs
@@ -17,8 +17,8 @@ public class StatisticsService : IStatisticsService
     {
         try
         {
-            string queriedYear = year <= 0 ? "" : $"&primary_release_year={year}";
-            string endpoint = $"{_settings.DiscoverPath}movie?include_adult=false&include_video=false{queriedYear}&language=en-US&page={page}&sort_by=vote_count.desc{_settings.AndQueryBuilder}";
+            var queriedYear = year <= 0 ? "" : $"&primary_release_year={year}";
+            var endpoint = $"{_settings.DiscoverPath}movie?include_adult=false&include_video=false{queriedYear}&language=en-US&page={page}&sort_by=vote_count.desc{_settings.AndQueryBuilder}";
             var movieListDto = await _service.GetAsync<MovieListDto>(endpoint);
             return _movieMapper.Map<MovieListDto, MovieList>(movieListDto);
         }
@@ -32,14 +32,35 @@ public class StatisticsService : IStatisticsService
     {
         try
         {
-            string queriedYear = year <= 0 ? "" : $"&primary_release_year={year}";
-            string endpoint = $"{_settings.DiscoverPath}movie?include_adult=false&include_video=false{queriedYear}&language=en-US&page={page}&sort_by=revenue.desc{_settings.AndQueryBuilder}";
+            var queriedYear = year <= 0 ? "" : $"&primary_release_year={year}";
+            var endpoint = $"{_settings.DiscoverPath}movie?include_adult=false&include_video=false{queriedYear}&language=en-US&page={page}&sort_by=revenue.desc{_settings.AndQueryBuilder}";
             var movieListDto = await _service.GetAsync<MovieListDto>(endpoint);
             return _movieMapper.Map<MovieListDto, MovieList>(movieListDto);
         }
         catch
         {
             return new MovieList();
+        }
+    }
+
+    public async Task<Credits> GetMostPopularMovieAsync(int personId)
+    {
+        try
+        {
+            var personCreditsDto = await _service.GetAsync<CreditsDto>(_settings.PersonPath + personId + _settings.MovieCredits + _settings.QueryBuilder);
+            var sortedCast = personCreditsDto.Cast.OrderByDescending(c => c.Popularity).ToList();
+            var sortedCrew = personCreditsDto.Crew.OrderByDescending(c => c.Popularity).ToList();
+            var sortedCredits = new CreditsDto
+            {
+                Cast = sortedCast,
+                Crew = sortedCrew
+            };
+            return _movieMapper.Map<CreditsDto, Credits>(sortedCredits);
+
+        }
+        catch
+        {
+            return new Credits();
         }
     }
 }
