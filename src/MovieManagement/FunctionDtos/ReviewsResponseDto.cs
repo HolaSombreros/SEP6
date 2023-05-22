ad2e58df0eb5ac92f2776bfd0052c03aa029f7b3
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-22 22:16:10 +0200

    Fix incorrect property name

--- src/MovieManagement/FunctionDtos/ReviewsResponseDto.cs
+++ src/MovieManagement/FunctionDtos/ReviewsResponseDto.cs
@@ -4,5 +4,5 @@ public class ReviewsResponseDto
 {
     public int TotalPages { get; set; }
     public int Page { get; set; }
-    public IEnumerable<ReviewResponseDto> ReviewReponseDtos { get; set; } = default!;
+    public IEnumerable<ReviewResponseDto> MovieRatingDtos { get; set; } = default!;
 }
