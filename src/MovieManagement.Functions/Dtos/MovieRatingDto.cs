8c19d3e3227a9be87e3b416989f461ed7b220985
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-23 12:38:18 +0200

    Revert "Remove unnecessary nullable annotations"
    This reverts commit 1128f11bf8943ec3cd6e049a209991d5428ef578.

--- src/MovieManagement.Functions/Dtos/MovieRatingDto.cs
+++ src/MovieManagement.Functions/Dtos/MovieRatingDto.cs
@@ -4,7 +4,7 @@ public class MovieRatingDto
 {
     public Guid Id { get; set; }
     public int Rating { get; set; }
-    public string Review { get; set; }
-    public DateTime CreatedDate { get; set; }
-    public UserDto CreatedBy { get; set; }
+    public string? Review { get; set; }
+    public DateTime? CreatedDate { get; set; }
+    public UserDto CreatedBy { get; set; } = null!;
 }
--- src/MovieManagement.Functions/Dtos/RatingDto.cs
+++ src/MovieManagement.Functions/Dtos/RatingDto.cs
@@ -2,9 +2,9 @@
 
 public class RatingDto
 {
-    public MovieDto MovieDto { get; set; }
+    public MovieDto MovieDto { get; set; } = default!;
     public int Rating { get; set; }
-    public string Review { get; set; }
+    public string Review { get; set; } = default!;
     public Guid UserId { get; set; }
     public Guid RatingId { get; set; }
 }
