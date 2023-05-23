c4ad48ed5cce1b38e2b6954edf3270987a8b2438
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-23 11:44:12 +0200

    Fix conversion of nullable DateTime

--- src/MovieManagement.Database/Entities/MovieEntity.cs
+++ src/MovieManagement.Database/Entities/MovieEntity.cs
@@ -5,7 +5,7 @@ public class MovieEntity
 {
     [Key]
     public int MovieId { get; set; }
-    public string? Title { get; set; }
+    public string Title { get; set; } = default!;
     public DateTime? ReleaseDate { get; set; }
     public string? PosterUrl { get; set; }
     public virtual ICollection<RatingEntity> Ratings { get; set; } = new List<RatingEntity>();
