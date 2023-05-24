9893de20f535bf2a7b8eb79baf50f65cd59aa411
Sombrero <adriana@grecea.net>
2023-05-24 17:52:02 +0200

    SEP6-123: add actor entity in dbcontext

---
+++ src/MovieManagement.Database/Entities/ActorEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace MovieManagement.Database.Entities; 
+
+[Table("Actor")]
+public class ActorEntity {
+    [Key]
+    public int ActorId { get; set; }
+
+    public int Gender { get; set; }
+    public DateTime Birthdate { get; set; }
+    public string Name { get; set; } = default!;
+}
