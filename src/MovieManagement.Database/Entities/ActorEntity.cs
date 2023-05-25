386d72c1d46b67d0e98f1cfa8762a37ccc2f440c
Sombrero <adriana@grecea.net>
2023-05-25 14:36:28 +0200

    SEP6-123: add service for actor and movie actor

--- src/MovieManagement.Database/Entities/ActorEntity.cs
+++ src/MovieManagement.Database/Entities/ActorEntity.cs
@@ -6,6 +6,6 @@ public class ActorEntity {
     public int ActorId { get; set; }
 
     public int Gender { get; set; }
-    public DateTime Birthdate { get; set; }
+    public DateTime? Birthdate { get; set; }
     public string Name { get; set; } = default!;
 }
---
+++ src/MovieManagement.Functions/Services/MovieActorService.cs
@@ -0,0 +1,31 @@
+﻿namespace MovieManagement.Functions.Services; 
+
+public class MovieActorService : IMovieActorService {
+    private readonly IMovieActorRepository _movieActorRepository;
+    private readonly IMapper _mapper;
+
+    public MovieActorService(IMapper mapper, IMovieActorRepository movieActorRepository) {
+        _mapper = mapper;
+        _movieActorRepository = movieActorRepository;
+    }
+
+    public async Task<List<MovieActorDto>> AddMovieActorsAsync(List<AddMovieActorDto> movieActorDtos, int actorId) {
+        var dtoList = new List<MovieActorDto>();
+        foreach (var movie in movieActorDtos) {
+            dtoList.Add(new MovieActorDto() {
+                ActorId = actorId,
+                MovieOrder = movie.MovieOrder,
+                MovieId = movie.MovieId
+            });
+        }
+        var list = _mapper.Map<List<MovieActorEntity>>(dtoList);
+        
+        foreach (var ma in list) {
+            if (await _movieActorRepository.GetAsync(ma.ActorId, ma.MovieId) is null) {
+                await _movieActorRepository.AddMovieActorAsync(ma);
+            }
+        }
+
+        return _mapper.Map<List<MovieActorDto>>(list);
+    }
+}
