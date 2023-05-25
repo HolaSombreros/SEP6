179ffc5f6139a55a0c69fe0b697fa99fbc9ea793
Stefania <72012292+stefaniatomuta@users.noreply.github.com>
2023-05-25 13:36:41 +0200

    SEP6-135: Removed unnecessary attributes

--- src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
+++ src/MovieManagement.TMDB.Api/Mappers/MovieMapper.cs
@@ -5,7 +5,7 @@ public class MovieMapper : Profile
     public MovieMapper()
     {
         CreateMap<MovieDto, Movie>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl));
-        CreateMap<MovieListDto, MovieList>().ForMember(dest => dest.ListType, opt => opt.MapFrom(src => "Upcoming"));
+        CreateMap<MovieListDto, MovieList>();
         CreateMap<DatePeriodDto, DatePeriod>();
         CreateMap<GenreDto, Genre>();
         CreateMap<CastDto, Cast>().ForMember(dest=> dest.ImageUrl, opt => opt.MapFrom(src => src.ImageUrl == null ? null : ApiConfig.ImageUri + src.ImageUrl))
