8f720999c4dea76af93df84cc781fc7256dc6d79
Sombrero <adriana@grecea.net>
2023-05-25 16:12:38 +0200

    SEP6-123: remove not implemented methods

--- src/MovieManagement.Database/Repositories/IMovieActorRepository.cs
+++ src/MovieManagement.Database/Repositories/IMovieActorRepository.cs
@@ -2,7 +2,5 @@
 
 public interface IMovieActorRepository {
     Task<MovieActorEntity?> AddMovieActorAsync(MovieActorEntity movieActorEntity);
-    Task<List<MovieEntity?>> GetMoviesForActor(int actorId);
-    Task<List<MovieActorEntity?>> GetMoviesRolesForActor(int actorId);
     Task<MovieActorEntity?> GetAsync(int actorId, int movieId);
 }
--- src/MovieManagement.Database/Repositories/MovieActorRepository.cs
+++ src/MovieManagement.Database/Repositories/MovieActorRepository.cs
@@ -14,14 +14,6 @@ public class MovieActorRepository : IMovieActorRepository {
         return await _repository.AddAsync(movieActorEntity);
     }
 
-    public async Task<List<MovieEntity?>> GetMoviesForActor(int actorId) {
-        throw new NotImplementedException();
-    }
-
-    public async Task<List<MovieActorEntity?>> GetMoviesRolesForActor(int actorId) {
-        throw new NotImplementedException();
-    }
-
     public async Task<MovieActorEntity?> GetAsync(int actorId, int movieId) {
         return await _context.MovieActors.FindAsync(actorId, movieId);
     }
