7503bcf2c68e5519c52db7dd3611e584d93852a6
Sombrero <adriana@grecea.net>
2023-05-25 14:35:23 +0200

    SEP6-123: add dtos

---
+++ src/MovieManagement.Functions/Dtos/MovieActorDto.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MovieManagement.Functions.Dtos; 
+
+public class MovieActorDto {
+    public int ActorId { get; set; }
+    public int MovieId { get; set; }
+    [JsonPropertyName("order")]
+    public int MovieOrder { get; set; }
+}
