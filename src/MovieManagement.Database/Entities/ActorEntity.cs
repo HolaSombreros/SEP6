a55746f7b6faf80ffe254e858e2d9fe782f0078b
Sombrero <adriana@grecea.net>
2023-05-25 16:11:15 +0200

    SEP6-123: remove space

--- src/MovieManagement.Database/Entities/ActorEntity.cs
+++ src/MovieManagement.Database/Entities/ActorEntity.cs
@@ -4,7 +4,6 @@
 public class ActorEntity {
     [Key]
     public int ActorId { get; set; }
-
     public int Gender { get; set; }
     public DateTime? Birthdate { get; set; }
     public string Name { get; set; } = default!;
