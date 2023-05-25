cefda0fb97eb6eb274494066c7f10e24705b95a9
RealFoXsTeRDOTA <foxsterdota@gmail.com>
2023-05-25 11:59:51 +0200

    Fix missing genres and incorrect json name

--- src/MovieManagement/FunctionDtos/ReviewResponseDto.cs
+++ src/MovieManagement/FunctionDtos/ReviewResponseDto.cs
@@ -2,6 +2,7 @@
 
 public class ReviewResponseDto
 {
+    [JsonPropertyName("ratingId")]
     public Guid Id { get; set; }
     public int Rating { get; set; }
     public string? Review { get; set; }
