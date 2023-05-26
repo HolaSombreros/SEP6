6a85bb891a513c5706123dc4a7e2754b1541963b
Sombrero <adriana@grecea.net>
2023-05-26 11:27:56 +0200

    SEP6-123: refactor requested changes

--- src/MovieManagement.Functions/Dtos/ActorDto.cs
+++ src/MovieManagement.Functions/Dtos/ActorDto.cs
@@ -2,8 +2,8 @@
 
 public class ActorDto {
     public int ActorId { get; set; }
-    public List<AddMovieActorDto> Movies { get; set; }
+    public List<AddMovieActorDto> Movies { get; set; } = default!;
     public int Gender { get; set; }
     public DateTime? Birthdate { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = default!;
 }
