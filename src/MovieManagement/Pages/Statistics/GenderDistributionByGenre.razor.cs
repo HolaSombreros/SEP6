912d52df57e82e79b339eccd1ae775f62d0e3e19
vladdirlea2002 <dirlea.ioan.vlad@gmail.com>
2023-05-29 12:23:00 +0200

    SEP6-139: Gender distribution FE

---
+++ src/MovieManagement/Pages/Statistics/GenderDistributionByGenre.razor.cs
@@ -0,0 +1,35 @@
+﻿namespace MovieManagement.Pages.Statistics;
+
+public partial class GenderDistributionByGenre : ComponentBase
+{
+    private GenderByGenreViewModel _viewModel = null!;
+    private List<Genre> _genres = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var genres = await GenreService.GetAllGenresAsync()!;
+        _genres = new List<Genre>();
+        foreach (var genre in genres.Genres!)
+        {
+            _genres.Add(genre!);
+        }
+        var data = await StatsService.GetGenderDistributionAsync(0);
+        _viewModel = new GenderByGenreViewModel(data);
+        StateHasChanged();
+    }
+    
+    private async Task GetGenderDistributionsAsync(ChangeEventArgs args)
+    {
+        try
+        {
+            var genreId = int.Parse(args.Value!.ToString()!);
+            var data = await StatsService.GetGenderDistributionAsync(genreId);
+            _viewModel = new GenderByGenreViewModel(data);
+        }
+        catch
+        {
+            _viewModel = null!;
+        }
+        StateHasChanged();
+    }
+}
--- src/MovieManagement/Services/IStatsService.cs
+++ src/MovieManagement/Services/IStatsService.cs
@@ -4,4 +4,5 @@ public interface IStatsService
 {
     Task<RatingDistributionByGenreModel> GetRatingDistributionByGenreAsync(int genreId);
     Task<AgeDistributionModel> GetAgeDistributionAsync(int movieId);
+    Task<GenderDistributionModel> GetGenderDistributionAsync(int genreId);
 }
